0d24ad05a3cd36bbc284c81b2c65163499bf9159
Masuduzzaman <md.santo0583210@gmail.com>
2025-12-07 19:28:10 +0600

    add mongodb code

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
@@ -7,3 +8,30 @@ const port = 3000;
 
 const app = express();
 app.use(cors());
+
+//missionscic
+//Tu7UlBdHGJyHUtDX
+
+
+
+const uri = "mongodb+srv://missionscic:Tu7UlBdHGJyHUtDXcluster0.o6mocqo.mongodb.net/?appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
